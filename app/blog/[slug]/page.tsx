f2128558e697861ddd65acd49f38aa58bb59af61
magnusquest <thisismartinc@gmail.com>
2025-05-04 20:33:19 -0400

    fix images

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,25 +3,24 @@ import { Content } from "@/models/types";
 import { getSinglePost } from "@/services/graphqlClient";
 
 export default async function SingleBlogPost({
-  params,
+	params
 }: {
-  params: { slug: string };
+	params: { slug: string };
 }) {
-  const post: Content = await getSinglePost(params.slug);
-  return (
-    <div className="flex flex-col items-center justify-center px-6 lg:px-40 py-20 md:mt-12">
-      <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
-      <Image
-        className="rounded-lg pb-5"
-        src={post.coverPhoto.url}
-        alt="cover photo"
-        width={800}
-        height={400}
-      />
-      <div
-        className="xl:px-28 pt-8 text-justify text-container"
-        dangerouslySetInnerHTML={{ __html: post.content.html }}
-      ></div>
-    </div>
-  );
+	const post: Content = await getSinglePost(params.slug);
+	return (
+		<div className="flex flex-col items-center justify-center px-6 lg:px-40 py-20 md:mt-12">
+			<h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+			<Image
+				className="rounded-lg pb-5"
+				src={post.coverPhoto.url}
+				alt="cover photo"
+				width={800}
+				height={400}
+			/>
+			<div
+				className="xl:px-28 pt-8 text-justify text-container"
+				dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
+		</div>
+	);
 }
--- next.config.js
+++ next.config.js
@@ -1,8 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-    domains: ['media.graphassets.com'],
-  },
-}
+	images: {
+		domains: [
+			"media.graphassets.com",
+			"us-east-1-shared-usea1-02.graphassets.com"
+		],
+		loader: "default" // Or create a custom loader if needed for specific Hygraph transformations
+	}
+};
 
-module.exports = nextConfig
+module.exports = {
+	...nextConfig,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "media.graphassets.com", // replace with your actual image host
+				port: "",
+				pathname: "/**"
+			},
+			{
+				protocol: "https",
+				hostname: "us-east-1-shared-usea1-02.graphassets.com",
+				port: "",
+				pathname: "/**"
+			}
+		]
+	}
+};
